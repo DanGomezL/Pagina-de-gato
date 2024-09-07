5d5976a0462024f6daf02fcabff72b5e35e91885
Daniel <daniel.g1998@outlook.es>
2024-09-06 23:06:59 -0400

    mi primer comentario

---
+++ script/script.js
@@ -0,0 +1,9 @@
+console.log("hola mundo")
+
+/*
+var caracteres = "abcdefghijkmnpqrtuvwxyzABCDEFGHJKMNPQRTUVWXYZ2346789";
+     var contraseña = "";
+ for (i=0; i<20; i++) contraseña +=caracteres.charAt(Math.floor(Math.random()*caracteres.length));
+console.log(contraseña)
+*/ 
+console.log("ADIOS")
